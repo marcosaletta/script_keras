e7cb6bad5f60e4694c5b8c237ab3660ce97b8856
marco_saletta <marco.saletta@banzai.it>
2016-10-03 10:38:16 +0200

    Add of keras_load_model_bigData.py to handle files line by lie

--- scripts/feature_sel_tree.py
+++ scripts/feature_sel_tree.py
@@ -1,5 +1,5 @@
 # Feature Importance with Extra Trees Classifier
-from pandas import read_csv
+#from pandas import read_csv
 from sklearn.ensemble import ExtraTreesClassifier
 import numpy
 import os
@@ -21,6 +21,7 @@ def usage(msg):
 #   print( "Usage: %s -i  input-file\n" % sys.argv[0]
    print("Usage: %s -f file input " % sys.argv[0])
    print("Usage: %s -o file output" % sys.argv[0])
+   print("Usage: %s -s number of features in the array" % sys.argv[0])
    print("Usage: %s -h help\n" % sys.argv[0])
    print("Example:python %s -f /home/marco/working-dir/Krux/anagrafica_files/v2/List4Keras_ALL_aud_200916_FEW_FEATURES_valid_10K4test_loadModel -o ../results/test_load_model_small_set -w ../models/model.h5_small_set -j ../models/model.json_small_set  \n"%sys.argv[0])
    raise SystemExit
@@ -30,7 +31,7 @@ def usage(msg):
 
 
 
-main(inFile,outFile):
+def main(inFile,outFile,setSize):
 # load data
 #url = "https://archive.ics.uci.edu/ml/machine-learning-databases/pima-indians-diabetes/pima-indians-diabetes.data"
 #names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
@@ -38,18 +39,25 @@ main(inFile,outFile):
 #array = dataframe.values
     logging.info("START")
     logging.info("LOAGING FEATURES FROM %s"%inFile)
+    logging.info("USING %i FEATURES"%setSize)
     array = numpy.loadtxt(inFile, delimiter=",")
-    X = array[:,0:8]
-    Y = array[:,8]
+    X = array[:,0:69]
+    Y = array[:,69]
 # feature extraction
     model = ExtraTreesClassifier()
-    model.fit(X, Y)
+    fit=model.fit(X, Y)
+    features = fit.transform(X)
+    print('--------------',features.shape)
     print(model.feature_importances_)
+    sort=numpy.sort(model.feature_importances_)
+    print(sort)
+    print(numpy.argsort(model.feature_importances_))
+    print(features[0:10,:])
     logging.info("END")
 
 
 if __name__=="__main__":
-    opts, args=getopt(sys.argv[1:],"f:o:h")
+    opts, args=getopt(sys.argv[1:],"f:o:s:h")
     opts=dict(opts)
     inFile=None
     outFile=None
@@ -57,9 +65,11 @@ if __name__=="__main__":
         inFile=str(opts['-f'])
     if '-o' in opts:
         outFile=str(opts['-o'])
+    if '-s' in opts:
+        setSize=int(opts['-s'])
     if '-h' in opts:
         usage('msg')
 #    if '-f' not in opts==True and '-o' not in opts==True and '-m' not in opts==True and '-j' not in opts==True and '-h' not in opts==True:
-    if '-f' not in opts and '-o' not in opts and '-h' not in opts:
+    if '-f' not in opts and '-o' not in opts and '-s' not in opts and '-h' not in opts:
         usage('0')
-    main(inFile,outFile)
+    main(inFile,outFile,setSize)
---
+++ scripts/keras_load_model_bigData.py
@@ -0,0 +1,110 @@
+import os
+import sys
+from getopt import getopt
+import logging
+from keras.models import Sequential
+from keras.layers import Dense
+from sklearn.cross_validation import StratifiedKFold
+import numpy
+from keras.models import model_from_json
+import random
+
+# fix random seed for reproducibility
+seed = 7
+numpy.random.seed(seed)
+
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+
+
+##########################################################################
+### usage
+##########################################################################
+def usage(msg):
+   if msg=='0':
+      print("=========================")
+      print("ERRORE: %s" % 'Missing variabile in input')
+      print("=========================")
+#   print( "Usage: %s -i  input-file\n" % sys.argv[0]
+   print("Usage: %s -f file input " % sys.argv[0])
+   print("Usage: %s -o file output" % sys.argv[0])
+   print("Usage: %s -w model file (model.h5)" % sys.argv[0])
+   print("Usage: %s -j json model file (model.json)" % sys.argv[0])
+   print("Usage: %s -h help\n" % sys.argv[0])
+   print("Example:python %s -f /home/marco/working-dir/Krux/anagrafica_files/v2/List4Keras_ALL_aud_200916_FEW_FEATURES_valid_10K4test_loadModel -o ../results/test_load_model_small_set -w ../models/model.h5_small_set -j ../models/model.json_small_set  \n"%sys.argv[0])
+   raise SystemExit
+
+
+#####################################################################
+
+
+
+def main(inFile,outFile,modelWeight,modelJson):
+
+    # load
+    logging.info("START")
+    logging.info("LOADING THE DATA SET")
+#    print(inFile)
+    #dataset = numpy.loadtxt(inFile, delimiter=",")
+    logging.info("LOADING MODEL")
+    with open(modelJson, 'r') as json_file:
+        loaded_model_json = json_file.read()
+    loaded_model = model_from_json(loaded_model_json)
+    # load weights into new model
+    logging.info("LOADING WEIGHTS IN NEW MODEL")
+    loaded_model.load_weights(modelWeight)
+    logging.info("LOADED MODEL FROM FILE")
+    #evaluating performance on the set
+    logging.info("COMPILING MODEL")
+    loaded_model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    file_user=open(inFile,'r')
+    fo=open(outFile, "w") as fo:
+    evaluate=True
+    right=0
+    wrong=0
+    for line in file_user:
+        array=numpy.fromstring(line, sep=',')
+    # split into input (X) and output (Y) variables
+        if len(array)==70:
+            X = array[0:69]
+            Y = array[70]
+        else:
+            X = array[0:69]
+            evaluate=False
+        predictions = loaded_model.predict(X,verbose=1)
+        to_write = [round(x) for x in predictions]
+        if evaluate==True
+            if to_write==Y:
+                right+=1
+            else:
+                wrong+=1
+#        print("%s: %.2f%%" % (loaded_model.metrics_names[1], score[1]*100))
+    #USARE QUESTI PER LA SCRITTURA DEL FILE CON LE PREVISION
+        else:
+            fo.write(to_write+"\n")
+    if evaluate==True:
+        perc_right=float(right)/float((right+wrong))
+        fo.write("Right:"+str(right)+"\nWrong:"+str(wrong)+"\nPercentual:"+str(perc_right))
+
+
+
+
+if __name__=="__main__":
+    opts, args=getopt(sys.argv[1:],"w:j:f:o:h")
+    opts=dict(opts)
+    inFile=None
+    outFile=None
+    if '-w' in opts:
+        modelWeight=str(opts['-w'])
+    if '-j' in opts:
+        modelJson=str(opts['-j'])
+    if '-f' in opts:
+        inFile=str(opts['-f'])
+    if '-o' in opts:
+        outFile=str(opts['-o'])
+    if '-h' in opts:
+        usage('msg')
+#    if '-f' not in opts==True and '-o' not in opts==True and '-m' not in opts==True and '-j' not in opts==True and '-h' not in opts==True:
+    if '-f' not in opts and '-o' not in opts and '-w' not in opts and '-j' not in opts and '-h' not in opts:
+        usage('0')
+    main(inFile,outFile,modelWeight,modelJson)
